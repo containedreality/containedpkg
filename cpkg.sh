c234b7703e77b2332913ce28d6fbaf2eb562fc72
containedreality <containedreality@users.noreply.github.com>
2023-01-22 15:59:08 -0500

    I suck at programming.

--- cpkg.sh
+++ cpkg.sh
@@ -4,8 +4,8 @@
 # configuration
 # export INSTALLDIR="$PWD"
 ROOT="$PWD" # Mainly used for testing
-CACHE="$PWD/etc/containedpkg/cache" # Path to list of avaliable packages.
-REMOVE_LOCATION="#PWD/var/containedpkg/" # Where remove.sh for packages go.
+CACHE="$ROOT/etc/containedpkg/cache" # Path to list of avaliable packages.
+REMOVE_LOCATION="$ROOT/var/containedpkg/" # Where remove.sh for packages go.
 REPOSITORY="http://localhost:8000"
 
 help() {
